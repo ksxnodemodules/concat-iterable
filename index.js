6d3ae3237d85e90225993bf3e0039617ea0dd2a6
khai96_ <hvksmr1996@gmail.com>
2017-04-07 20:15:11 +0700

    Run standard --fix

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 
 ((module) => {
-	'use strict';
+  'use strict'
 
-	var recursiveConstructor = require('x-iterable-utils/recursive-constructor');
-	var manySameElements = require('x-iterable-utils/many-same-elements');
-	var ConcatIterableSuper = require('x-iterable-utils/appx-super-class')(build, iterate);
+  var recursiveConstructor = require('x-iterable-utils/recursive-constructor')
+  var manySameElements = require('x-iterable-utils/many-same-elements')
+  var ConcatIterableSuper = require('x-iterable-utils/appx-super-class')(build, iterate)
 
-	class ConcatIterable extends ConcatIterableSuper {
+  class ConcatIterable extends ConcatIterableSuper {
+    static times (...args) {
+      return ConcatIterable.multiply(...args)
+    }
 
-		static times(...args) {
-			return ConcatIterable.multiply(...args);
-		}
-
-		static multiply(iterable, times) {
-			var args = manySameElements(iterable, times);
-			return args.length ? new ConcatIterable(...args) : [];
-		}
-
+    static multiply (iterable, times) {
+      var args = manySameElements(iterable, times)
+      return args.length ? new ConcatIterable(...args) : []
+    }
 	}
 
-	module.exports = ConcatIterable;
+  module.exports = ConcatIterable
 
-	function build(self, ...args) {
-		return recursiveConstructor(self, ConcatIterable, (value) => value, ...args);
-	}
+  function build (self, ...args) {
+    return recursiveConstructor(self, ConcatIterable, (value) => value, ...args)
+  }
 
-	function * iterate() {
-		yield * this.first;
-		yield * this.second;
-	}
-
-})(module);
+  function * iterate () {
+    yield * this.first
+    yield * this.second
+  }
+})(module)
